5b88922c70577d116cf79668349427e879c89f24
Laurence Goolsby <laurencegoolsby@navapbc.com>
2026-04-10 16:47:39 -0400

    Set log_config=None on uvicorn.run() to disable uvicorn's default handlers

--- template/{{app_name}}/src/documentai_api/main.py
+++ template/{{app_name}}/src/documentai_api/main.py
@@ -10,7 +10,7 @@ def main():  # pragma: no cover
         host = os.getenv("HOST", "127.0.0.1")
         port = int(os.getenv("PORT", 8000))
 
-        uvicorn.run("documentai_api.app:app", host=host, port=port, reload=False)
+        uvicorn.run("documentai_api.app:app", host=host, port=port, reload=False, log_config=None)
 
 
 if __name__ == "__main__":
