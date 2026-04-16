648fa4cdc37e59d1eae7db4b953613224480db86
Laurence Goolsby <laurencegoolsby@navapbc.com>
2026-04-16 17:05:45 -0400

    Update main.py to use  __package__ for logging context, add test-audit make target

--- template/{{app_name}}/src/documentai_api/main.py
+++ template/{{app_name}}/src/documentai_api/main.py
@@ -6,7 +6,7 @@ from documentai_api.logging.config import LoggingContext
 
 
 def main() -> None:  # pragma: no cover
-    with LoggingContext("documentai_api"):
+    with LoggingContext(__package__):
         host = os.getenv("HOST", "127.0.0.1")
         port = int(os.getenv("PORT", 8000))
 
